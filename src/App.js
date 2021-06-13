d1ab06d74959347b06146fe81413baa4af562b05
haneenbader <haneenalwatan993@gmail.com>
2021-06-13 17:56:27 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+ import axios from 'axios';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      cityName: ''
+    }
+  };
+
+
+  updateCityNameState = (e) => {
+    this.setState({
+      cityName: (e.target.value)
+    });
+  }
+
+
+getCityData = async (e)=> {
+  e.preventdefault();
+ 
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <h1> Enter City Explorer</h1>
+        <form onSubmit={this.getCityData}>
+          <label> City Name  </label>
+          <input type="text" onChange={this.updateCityNameState} > </input>
+          <br> </br>
+          <input type="submit" value="git City" > Explor </input>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App 
+
+
