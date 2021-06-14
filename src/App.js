f9d4c546d93565440af24a56489a5eb7d0e2c939
haneenbader <haneenalwatan993@gmail.com>
2021-06-15 00:37:02 +0300

    add link lat and lon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-//  import axios from 'axios';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -12,7 +12,6 @@ class App extends Component {
     }
   };
 
-
   updateCityNameState = (e) => {
     this.setState({
       cityName: (e.target.value)
@@ -22,7 +21,7 @@ class App extends Component {
 
   getCityData = async (e) => {
     e.preventdefault();
-    const axionResponce = Await axios.get()
+    const axionResponce = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.aa1e1f18e47ddfdc3b6905e918e424b6&q=${this.state.cityName}&format=json`);
     this.setState({
       cityData: axionResponce.data[0],
       displayData: true
@@ -35,14 +34,14 @@ class App extends Component {
         <h1> Enter City Explorer</h1>
         <form onSubmit={this.getCityData}>
           <label> City Name  </label>
-          <input type="text" onChange={this.updateCityNameState} > </input>
-          <br> </br>
-          <input type="submit" value="git City" > Explor </input>
+          <input type="text" onChange={this.updateCityNameState} /> 
+          <br></br>
+          <button type="submit" value="git City" > Explor! </button>
         </form>
         {this.state.displayData &&
           <div>
-            <p> {this.state.cityName.display_name}</p>
-            <img src={}/>
+            <p> {this.state.cityName.display_name } is located at {this.state.data.lat} by {this.state.data.lon} </p>
+            {/* <img src={``} alt='' /> */}
           </div>
         }
       </div>
